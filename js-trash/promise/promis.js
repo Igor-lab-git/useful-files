d2fbfb0d06180f72e5b5d78346a2f8a3758a019b
Igor <igorkost142@gmail.com>
2025-10-06 16:43:09 +0300

    fix-15

--- js-trash/promise/promis.js
+++ js-trash/promise/promis.js
@@ -5,3 +5,30 @@ fetch('https://jsonplaceholder.typicode.com/posts')
   .then((response) => response.json())
   .then((json) => console.log(json))
   .catch(error => console.log(error.message))
+
+  
+const promis = new Promise((resolve, reject) => {
+  if(Math.random() > 0.5) {
+    resolve("Ура успех");
+  } else {
+    reject("Увы осечка");
+  }
+})
+
+promis
+.then((data) => console.log("Полученные данные:", data))
+.catch((error) => console.log("Полученные данные:", error))
+.finally(() => console.log("Конец загрузки:"))
+
+
+
+async function loadJson(url) {
+  try {
+    let response = await fetch(url);
+    return response.json()
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+loadJson('no-such-user.json') // (3)
