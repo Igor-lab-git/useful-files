b274e0e364240eb32c5de8b8a56fee14f111c62b
Igor-lab-git <igorkost142@gmail.com>
2025-10-12 21:43:36 +0300

    fix-21

--- js-trash/fetch/index.js
+++ js-trash/fetch/index.js
@@ -1,3 +1,72 @@
-document.body.style.background = "red";
+// const divElement = document.createElement("div");
+//
+// divElement.style.width = "200px";
+// divElement.style.height = "200px";
+// divElement.style.border = "1px solid red";
+// window.document.body.appendChild(divElement);
+//
+// console.log(navigator.userAgent);
+// console.log(location.href);
+//
+// window.localStorage.setItem("user", JSON.stringify({id: 1, name: "Igor", "age": 36 }));
+// const user = JSON.parse(localStorage.getItem("user"));
+// console.log(user);
+//
+// console.log(document);
+//
+// const innerWidth = 10;
+// const innerHeight = 20;
+// console.log("Ширина", window.innerWidth);
+// console.log("Высота", window.innerHeight);
+// console.log(innerHeight);
+// console.log(innerHeight);
 
-setTimeout(() => document.body.style.background = "", 2000)
+console.log(document.body.parentElement);
+console.log(document.body.previousElementSibling);
+console.log(document.body.nextElementSibling);
+console.log(document.body.children);
+const bodyChildren = document.body.children;
+console.log(bodyChildren);
+
+const button = document.createElement('button');
+document.body.append(button);
+
+console.log(bodyChildren)
+
+// const arrayBodyChildren = [...bodyChildren];
+// arrayBodyChildren.forEach((element) => {
+//     console.log(element)
+// })
+console.log(bodyChildren[0]);
+console.log(document.body.lastElementChild);
+console.log(document.body.lastChild);
+console.log(document.body.firstChild);
+console.log(document.body.firstElementChild);
+
+const content = document.getElementById("content");
+content.style.backgroundColor = "green";
+
+const list = document.querySelectorAll("#list > li");
+
+
+const arrayList = [...list].forEach((el) => console.log(el.textContent));
+
+const newButton = document.querySelector("#button");
+
+const link = document.querySelector("#link");
+const isParent = link.closest("#list");
+console.log(isParent);
+const allInput = document.querySelectorAll("input");
+for(let inputItem of [...allInput]) {
+    console.log(inputItem.value);
+}
+
+console.log(allInput[0].value = "Igor")
+
+const newAllInput = document.getElementsByTagName("input");
+console.log(typeof newAllInput)
+console.log(typeof allInput)
+
+console.log(window)
+
+
